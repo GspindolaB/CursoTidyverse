84df8e82cdf781033375a324ddaa6e3153c97b70
unknown <gspindolab>
2021-08-11 17:52:02 -0500

    Trabajando con geometrias

--- 01VisualizacionDatos/06geometriasggplot.R
+++ 01VisualizacionDatos/06geometriasggplot.R
@@ -5,3 +5,30 @@ ggplot(data = vehiculos) +
 ggplot(data = vehiculos) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(.~drv)
+
+### GEOMETRIAS DE GGPLOT ###
+
+#Geometría de puntos
+ggplot(data = vehiculos) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+#Geometría de smooth
+ggplot(data = vehiculos) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = vehiculos) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv, color = class))
+
+#Geometría de lineas
+ggplot(data = vehiculos) +
+  geom_line(mapping = aes(x = displ, y = hwy))
+
+#Mezcla de geometrias
+ggplot(data = vehiculos) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv, color = class))
+
+?geom_smooth
+
+
+
