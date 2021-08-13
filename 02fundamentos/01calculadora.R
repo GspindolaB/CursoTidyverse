7baf75db59f5f3d8260f94428865cfb4efc47a94
unknown <gspindolab>
2021-08-13 16:53:13 -0500

    R como calculadora

---
+++ 02fundamentos/01calculadora.R
@@ -0,0 +1,52 @@
+### R COMO CALCULADORA ###
+
+5 + 5 +5
+
+(25*85)/7
+
+#la función round nos permite redondear un número
+round((25*85)/7, 2)
+
+#la funcion pi nos permite obtener el valor de pi
+pi
+
+round(pi,4)
+
+#la funcion sqrt nos calcula la raiz cuadrada de un numero
+sqrt(8)
+
+#tambien podemos utilizar funciones trigonometricas como seno, coseno y tangente
+sin(pi/2)
+
+tan(pi/4)
+
+cos(pi/8)
+
+#El numero e lo podemos saber con la funcion exp
+exp(1)
+exp(2) #numero e al cuadrado
+exp(3) #numero e al cubo
+
+#También podemos saber logaritmos
+log(7, base = 3)
+log(7, base = 8)
+
+#Podemos elevar a una potencia utilizando el simbolo de ^
+
+2^3 # 2 al cubo
+
+5^5 # 5 elevado a la quinta potencia
+
+#El factorial lo podemos saber utiliza do la funcion factorial
+factorial(5)
+
+#choose nos sirve para elegir las combinaciones posibles de un numero de elementos
+choose(9,3) #En este caso para 9 opciones dadas con 3 posibilidades cada una, existen 84 combinaciones posibles
+
+#Eso y muchas cosas se pueden hacer para utilizar R cómo calculadora
+
+#Creacion de variables
+
+a = 5*2
+b <- sqrt(32)
+round(b,2) -> c
