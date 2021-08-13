7208ee0647e0656147f66e6f8f95ccd5252b2ea5
unknown <gspindolab>
2021-08-13 17:04:29 -0500

    Variables, nada más

---
+++ 02fundamentos/02variables.R
@@ -0,0 +1,19 @@
+### VARIABLES ###
+
+# El nombre de una variable debe de comenzar letra o caracter nunca con un numero
+
+# Se pueden crear variables con la notacion de snake_case y se veria de la siguiente manera.
+# Por cada nueva palabra se agrega un guion bajo
+
+raiz_cuadrada <- sqrt(5)
+
+# También se puede utilizar la notación de camello. Cada nueva palabra comienza con mayusculas
+
+potenciaNumero <- 9^3
+
+# Tambien se le puede dar nombre a una variable separando las palabras con puntos 
+
+factorial.numero <- factorial(15)
+
+# De igual forma se puede hacer una combinación de las notaciones pero no es lo recomendado
+Variable.separada_por_Todo <- "Hola"
