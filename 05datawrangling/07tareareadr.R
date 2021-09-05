667c289de5444b3476c3c6d6bb46dd1e92660735
unknown <gspindolab>
2021-09-05 12:23:08 -0500

    Subiendo está tarea

---
+++ 05datawrangling/07tareareadr.R
@@ -0,0 +1,15 @@
+?read_fwf
+
+read_csv("x,y\n1,2,3\n4,5,6")
+
+read_csv("x,y\n1,'a,b'")
+
+read_csv("x,y,z\n1,2\n3,4,5,6")
+
+read_csv("x,y\n\"1")
+
+read_csv("x,y\n1,2\na,b")
+
+read_csv("x;y\n1;2")
+
+read_csv2("x;y\n1;2")
